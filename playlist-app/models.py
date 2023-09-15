025883414f4a12da04ee7289a46727603ebc84a5
Dorion Mitchell <dorionmitchell@Dorions-MBP.local>
2023-09-15 15:00:22 -0400

    created modals for tables. Reorganized files to get app to run

--- playlist-app/models.py
+++ playlist-app/models.py
@@ -8,20 +8,31 @@ db = SQLAlchemy()
 class Playlist(db.Model):
     """Playlist."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = "playlist"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
 
 
 class Song(db.Model):
     """Song."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__= "song"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    artist = db.Column(db.Text, nullable=False)
 
 
 class PlaylistSong(db.Model):
     """Mapping of a playlist to a song."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__= "playlist_song"
 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey("playlist.id"))
+    song_id = db.Column(db.Integer, db.ForeignKey("song.id"))
 
 # DO NOT MODIFY THIS FUNCTION
 def connect_db(app):
